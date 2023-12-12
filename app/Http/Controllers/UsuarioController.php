9454037c43616a28c289ac0f58ee436375323674
BrandonRv <138633220+BrandonRv@users.noreply.github.com>
2023-12-12 02:36:38 -0500

    Admin-Usuarios

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Usuario::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $usuario =  new Usuario();
+        $usuario->nombre = $request->nombre;
+        $usuario->apellido = $request->apellido;
+        $usuario->correo_electronico = $request->correo_electronico;
+        $usuario->fecha = $request->fecha;
+        $usuario->save();
+        return "El Usuario se guardo correctamente";
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        return Usuario::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $usuario = Usuario::find($id);
+        $usuario->nombre = $request->nombre;
+        $usuario->apellido = $request->apellido;
+        $usuario->correo_electronico = $request->correo_electronico;
+        $usuario->fecha = $request->fecha;
+        $usuario->save();
+        return  "El Usuario se Actualizo correctamente";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $usuario = Usuario::find($id);
+        $usuario->delete();
+        return  "El Usuario se Elimino correctamente";
+    }
+}
